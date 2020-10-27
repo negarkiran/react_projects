ae26062e5bcadf3c0487bc4e97584b4ea8bb2c13
Kiran Hegde <khegde@equalexperts.com>
2020-10-27 18:59:05 +0530

    add router changes

--- learn_german/src/components/AddWord.js
+++ learn_german/src/components/AddWord.js
@@ -1,18 +1,49 @@
 import React, { useState, useEffect } from "react";
+import germanapi from "../api/german";
 
-const AddWord = ({ wordAdded, onAdd }) => {
+const AddWord = () => {
   const [english, setEnglish] = useState("");
   const [german, setGerman] = useState("");
+  const [showMessage, setShowMessage] = useState(false);
+  const [message, setMessage] = useState("");
 
   useEffect(() => {
-    setEnglish('');
-    setGerman('');
-  }, [wordAdded]);
+    setEnglish("");
+    setGerman("");
+  }, [showMessage]);
+
+  const isEmpty = (str) => {
+    return (!str || 0 === str.trim().length);
+  }
+
+  const onAdd = async (english, german) => {
+    if (isEmpty(english) || isEmpty(german)) {
+      setShowMessage(true);
+      setEnglish('');
+      setGerman('');
+      setMessage("Please enter valid words");
+    } else {
+      const response = await germanapi.post("words", {
+        english: english,
+        german: german,
+      });
+  
+      if (response.status === 200) {
+        setShowMessage(true);
+        setMessage("Word Added Successfully");
+      }
+    }
+  };
 
   return (
     <div>
-      <h1 className="ui header center aligned">Learn German</h1>
       <div className="ui segment">
+        {showMessage && (
+          <div className="ui info message">
+            <i className="close icon" onClick={(e) => setShowMessage(false)}></i>
+            <div className="header">{message}</div>
+          </div>
+        )}
         <div className="ui input fluid ">
           <input
             type="text"
